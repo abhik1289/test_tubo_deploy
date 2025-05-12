acf374e6d4b60fe8a94f830401206ea52d8ebe06
Abhik <theabhik2020@gmail.com>
2025-05-12 17:27:16 +0530

    main: Added

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,16 @@
-// console.log("HELLO");
+// index.ts
 
 import express, { Request, Response } from "express";
-// import { NextRequest } from "express";
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-     res.json({ message: "Hello" })
-})
+  res.json({ message: "Hello" });
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on http://localhost:${5000}`);
+});
