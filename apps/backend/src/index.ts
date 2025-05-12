2da9fe44530edf9d8f8e0e2312f1f7a1ec3e64f5
Abhik <theabhik2020@gmail.com>
2025-05-12 17:20:24 +0530

    main: Added

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,6 @@ import express, { Request, Response } from "express";
 // import { NextRequest } from "express";
 const app = express();
 app.use(express.json());
-app.post("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
      res.json({ message: "Hello" })
 })
