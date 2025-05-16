74c6d2c1b2c5db33388f1e6e3bf9b6e5fd44acc3
Abhik <theabhik2020@gmail.com>
2025-05-16 08:12:33 +0530

    main: Added

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Turbo + Express + Vercel!" });
+  res.send("HELLO");
 });
 
 export default serverless(app);
